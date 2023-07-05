12a6cc482cabc0aef1cccca52f274ebcf9743cef
singhraj5676 <singhraj5676@gmail.com>
2023-07-05 12:01:47 +0530

    Final Changes

--- todolist/settings.py
+++ todolist/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8763sj#%-$u12fz5nklpf4)hh5cv=#%rrk$!ctbe4^0z^7+$3d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = False               
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
