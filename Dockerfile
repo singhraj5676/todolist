ff09f2849b03ea0bdaeaf8acf245972a5ec904fc
singhraj5676 <singhraj5676@gmail.com>
2023-07-05 14:57:35 +0530

    Final Changes

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ WORKDIR /Demo
 ADD . /Demo
 RUN pip install -r requirements.txt
 EXPOSE 8000
-CMD python manage.py runserver 0:8000
+CMD python manage.py runserver
 
+proxy_pass http://127.0.0.1:8000;
--- todolist/settings.py
+++ todolist/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS: True
+CORS_URLS_REGEX = r"^/api/.*$"
